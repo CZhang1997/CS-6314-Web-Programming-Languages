1d2f2a0c2b55d52641a6046e35886018ae5b3b89
Churong Zhang <cxz173430@utdallas.edu>
2021-02-27 10:46:30 -0600

    pw3

---
+++ practices/PW3-cxz173430/index.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+  $("span").hide();
+  $("#new").hide();
+  $("#add").click(function () {
+    $("ul").append("<li>" + $("#new").val() + "</li>");
+  });
+
+  $("#add-icon").click(function () {
+    $("#new").toggle();
+    $("#new").focus();
+  });
+
+  $("#new").keypress(function (event) {
+    if (event.which === 13) {
+      $("ul").append(
+        '<li> <span><i class="fa fa-trash"></i></span><div class="todos">' +
+          $("#new").val() +
+          "</div></li>"
+      );
+      $("#new").val("");
+      $("span").hide();
+    }
+  });
+
+  $("ul").on("click", "li", function () {
+    $(this).toggleClass("done");
+  });
+  $("ul").on("click", "span", function () {
+    $(this).parent().remove();
+  });
+
+  $("ul").on("mouseenter", "li", function () {
+    $(this).find("span").show();
+  });
+  $("ul").on("mouseleave ", "li", function () {
+    $(this).find("span").hide();
+  });
+});
